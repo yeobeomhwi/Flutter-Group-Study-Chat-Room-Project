0fbe007db76c22d3e845f56d4ee6149b3a021a2d
여 범휘 <yeobh0828@naver.com>
2024-10-18 16:37:43 +0900

    Feat : Badge추가

--- lib/Screen/filteringRoom/s_filteringMainScreen.dart
+++ lib/Screen/filteringRoom/s_filteringMainScreen.dart
@@ -1,9 +1,80 @@
 import 'package:app_team2/components/w_topicMenuCard.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class FilteringMainScreen extends StatelessWidget {
+class FilteringMainScreen extends StatefulWidget {
   const FilteringMainScreen({super.key});
+
+  @override
+  State<FilteringMainScreen> createState() => _FilteringMainScreenState();
+}
+
+class _FilteringMainScreenState extends State<FilteringMainScreen> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance; // Firestore 인스턴스
+  bool _isLoading = true; // 로딩 상태
+
+  Map<String, int> _topicCounts = {}; // 주제별 방 개수
+
+
+  @override
+  void initState() {
+    super.initState();
+    _loadInitialRooms(); // 초기 방 로드
+  }
+
+  Future<void> _loadInitialRooms() async {
+    setState(() {
+      _isLoading = true; // 로딩 상태 활성화
+    });
+
+    // 주제별 방 개수 초기화
+    Map<String, int> topicCounts = {
+      '수학': 0,
+      '영어': 0,
+      '프로그래밍': 0,
+      '디자인': 0,
+      '스포츠': 0,
+      '과학': 0,
+      '독서': 0,
+      '자격증': 0,
+    };
+
+    // 각 주제에 대한 문서 가져오기
+    for (String topic in topicCounts.keys) {
+      QuerySnapshot snapshot = await _firestore
+          .collection('study_rooms')
+          .where('topic', isEqualTo: topic)
+          .get();
+
+      // 주제별 방 개수 업데이트
+      topicCounts[topic] = snapshot.docs.length; // 주제에 대한 문서 개수
+    }
+
+    // 방 목록 가져오기
+    QuerySnapshot allRoomsSnapshot = await _firestore
+        .collection('study_rooms')
+        .orderBy('createDate', descending: true) // 생성 날짜 기준 정렬
+        .get();
+
+    // 주제별 방 개수 상태 업데이트
+    _topicCounts = topicCounts;
+
+    // 디버깅: 주제별 개수 출력
+    print('각 주제별 방 개수: $_topicCounts');
+
+    setState(() {
+      _isLoading = false; // 로딩 상태 비활성화
+    });
+  }
+
+  // 주제 카드 행 빌드 함수
+  Widget _buildTopicRow(List<TopicMenuCard> cards) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: cards,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,98 +84,74 @@ class FilteringMainScreen extends StatelessWidget {
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        // 스크롤 가능
+      body: _isLoading // 로딩 상태에 따라 다르게 표시
+          ? Center(child: CircularProgressIndicator()) // 로딩 인디케이터
+          : SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(height: 20),
+              _buildTopicRow([
+                TopicMenuCard(
+                  title: '수학',
+                  uri: 'assets/images/topic/Mathematics.png',
+                  onCardTap: () => GoRouter.of(context).push('/Math'),
+                  badgeCount: _topicCounts['수학'] ?? 0, // 방 개수 추가
+                ),
+                TopicMenuCard(
+                  title: '영어',
+                  uri: 'assets/images/topic/English.png',
+                  onCardTap: () => GoRouter.of(context).push('/Eng'),
+                  badgeCount: _topicCounts['영어'] ?? 0, // 방 개수 추가
+                ),
+              ]),
               const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TopicMenuCard(
-                    title: '수학',
-                    uri: 'assets/images/topic/Mathematics.png',
-                    onCardTap: () {
-                      GoRouter.of(context).push('/Math'); // MathScreen으로 이동
-                    },
-                  ),
-                  TopicMenuCard(
-                    title: '영어',
-                    uri: 'assets/images/topic/English.png',
-                    onCardTap: () {
-                      GoRouter.of(context).push('/Eng'); // EnglishScreen으로 이동
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20), // 행 사이 간격
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TopicMenuCard(
-                    title: '프로그래밍',
-                    uri: 'assets/images/topic/programming.png',
-                    onCardTap: () {
-                      GoRouter.of(context)
-                          .push('/Program'); // ProgrammingScreen으로 이동
-                    },
-                  ),
-                  TopicMenuCard(
-                    title: '디자인',
-                    uri: 'assets/images/topic/Design.png',
-                    onCardTap: () {
-                      GoRouter.of(context).push('/Design'); // DesignScreen으로 이동
-                    },
-                  ),
-                ],
-              ),
+              _buildTopicRow([
+                TopicMenuCard(
+                  title: '프로그래밍',
+                  uri: 'assets/images/topic/programming.png',
+                  onCardTap: () => GoRouter.of(context).push('/Program'),
+                  badgeCount: _topicCounts['프로그래밍'] ?? 0, // 방 개수 추가
+                ),
+                TopicMenuCard(
+                  title: '디자인',
+                  uri: 'assets/images/topic/Design.png',
+                  onCardTap: () => GoRouter.of(context).push('/Design'),
+                  badgeCount: _topicCounts['디자인'] ?? 0, // 방 개수 추가
+                ),
+              ]),
               const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TopicMenuCard(
-                    title: '스포츠',
-                    uri: 'assets/images/topic/Sports.png',
-                    onCardTap: () {
-                      GoRouter.of(context).push('/Sports'); // SportsScreen으로 이동
-                    },
-                  ),
-                  TopicMenuCard(
-                    title: '과학',
-                    uri: 'assets/images/topic/Science.png',
-                    onCardTap: () {
-                      GoRouter.of(context)
-                          .push('/Science'); // ScienceScreen으로 이동
-                    },
-                  ),
-                ],
-              ),
+              _buildTopicRow([
+                TopicMenuCard(
+                  title: '스포츠',
+                  uri: 'assets/images/topic/Sports.png',
+                  onCardTap: () => GoRouter.of(context).push('/Sports'),
+                  badgeCount: _topicCounts['스포츠'] ?? 0, // 방 개수 추가
+                ),
+                TopicMenuCard(
+                  title: '과학',
+                  uri: 'assets/images/topic/Science.png',
+                  onCardTap: () => GoRouter.of(context).push('/Science'),
+                  badgeCount: _topicCounts['과학'] ?? 0, // 방 개수 추가
+                ),
+              ]),
               const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TopicMenuCard(
-                    title: '독서',
-                    uri: 'assets/images/topic/Reading.png',
-                    onCardTap: () {
-                      GoRouter.of(context)
-                          .push('/Reading'); // ReadingScreen으로 이동
-                    },
-                  ),
-                  TopicMenuCard(
-                    title: '자격증',
-                    uri: 'assets/images/topic/Certificate.png',
-                    onCardTap: () {
-                      GoRouter.of(context)
-                          .push('/Certificate'); // CertificateScreen으로 이동
-                    },
-                  ),
-                ],
-              ),
+              _buildTopicRow([
+                TopicMenuCard(
+                  title: '독서',
+                  uri: 'assets/images/topic/Reading.png',
+                  onCardTap: () => GoRouter.of(context).push('/Reading'),
+                  badgeCount: _topicCounts['독서'] ?? 0, // 방 개수 추가
+                ),
+                TopicMenuCard(
+                  title: '자격증',
+                  uri: 'assets/images/topic/Certificate.png',
+                  onCardTap: () => GoRouter.of(context).push('/Certificate'),
+                  badgeCount: _topicCounts['자격증'] ?? 0, // 방 개수 추가
+                ),
+              ]),
             ],
           ),
         ),
--- lib/components/w_topicMenuCard.dart
+++ lib/components/w_topicMenuCard.dart
@@ -1,29 +1,26 @@
 import 'package:flutter/material.dart';
 
-class TopicMenuCard extends StatefulWidget {
+class TopicMenuCard extends StatelessWidget {
   final String title; // 카드 제목
   final String uri; // 이미지 URI
   final Function() onCardTap; // 수정: 콜백 타입 수정
+  final int badgeCount; // 추가: 방 개수
 
   const TopicMenuCard({
     super.key,
     required this.title,
     required this.uri,
-    required this.onCardTap, // 수정: onCardTap 매개변수 추가
+    required this.onCardTap,
+    required this.badgeCount, // 방 개수 추가
   });
 
-  @override
-  State<TopicMenuCard> createState() => _TopicMenuCardState();
-}
-
-class _TopicMenuCardState extends State<TopicMenuCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 180, // 카드 너비
-      height: 200, // 카드 높이
+      height: 160, // 카드 높이
       child: GestureDetector(
-        onTap: widget.onCardTap, // 수정: widget.onCardTap으로 수정
+        onTap: onCardTap,
         child: Card(
           elevation: 4, // 카드 그림자 효과
           shape: RoundedRectangleBorder(
@@ -31,32 +28,46 @@ class _TopicMenuCardState extends State<TopicMenuCard> {
           ),
           child: Stack(
             children: [
-              // 이미지 위젯
               ClipRRect(
                 borderRadius: BorderRadius.circular(10), // 모서리 둥글게
                 child: Image.asset(
-                  widget.uri,
-                  fit: BoxFit.cover, // 이미지 크기 조정
-                  height: 200, // 카드 높이에 맞춤
-                  width: double.infinity, // 카드 너비
+                  uri,
+                  fit: BoxFit.cover,
+                  height: 200,
+                  width: double.infinity,
                 ),
               ),
-              // 이미지 위에 텍스트 표시
               Positioned(
-                top: 20, // 상단 여백
+                top: 20,
                 left: 0,
                 right: 0,
                 child: Center(
                   child: Container(
-                    color: Colors.black54, // 텍스트 배경색
-                    padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16), // 텍스트 패딩
+                    color: Colors.black54,
+                    padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                     child: Text(
-                      widget.title,
+                      title,
                       style: TextStyle(
-                        color: Colors.white, // 텍스트 색상
-                        fontSize: 20, // 텍스트 크기
-                        fontWeight: FontWeight.bold, // 텍스트 굵게
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
+                    ),
+                  ),
+                ),
+              ),
+              // Badge 추가
+              Positioned(
+                top: 10,
+                right: 10,
+                child: CircleAvatar(
+                  backgroundColor: Colors.red,
+                  radius: 12,
+                  child: Text(
+                    badgeCount.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
                     ),
                   ),
                 ),
